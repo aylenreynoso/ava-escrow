080fd7007380ee96bc4732fbb9cdcd18c52483a9
Aylen Reynoso <areynoso@fi.uba.ar>
2024-10-20 02:18:26 -0300

    init

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ava Escrow",
+  description: "description place holder",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThirdwebProvider>{children}</ThirdwebProvider>
+      </body>
+    </html>
+  );
+}
